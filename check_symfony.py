b2388049ea7111ad8224cef5a8f75a8d43489b78
bizi <tristanbizien@gmail.com>
2021-01-19 13:07:35 +0100

    FIX Header request OS Windows

--- check_symfony.py
+++ check_symfony.py
@@ -35,7 +35,7 @@ args=parser.parse_args()
 
 headers_Get = {
         'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
-		'accept-encoding' : 'gzip, deflate, br',
+		'accept-encoding' : 'gzip, deflate',
 		'accept-language' : 'fr-FR,fr;q=0.9,en-US;q=0.8,en;q=0.7',
 		'cookie': args.cookie,
 		'sec-fetch-dest' : 'document',
